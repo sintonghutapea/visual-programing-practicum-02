410b048d5a2efc7584102d0eb61616e5e1d5600b
sintonghutapea <sintonghutapea123@gmail.com>
2023-09-30 22:21:19 +0700

    Sudah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,36 @@
-// NIM - Your Name
+// 12S23029 - Sintong Hutapea
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama, kode, matkul, dosen, deadline, nim, status, hasil1, hasil2, hasil3;
+        double tingkat, hari, prioritas;
+
+        nama = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        deadline = input.nextLine();
+        nim = input.nextLine();
+        tingkat = input.nextDouble();
+        hari = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkat * (1.0 / hari);
+        System.out.println("Prioritas : " + prioritas);
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                hasil1 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(nama + "|" + kode + "|" + matkul + "|" + dosen + "|" + deadline + "|" + nim + "|" + tingkat + "|" + hari + "|" + status + "|" + hasil1);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,33 @@
-//NIM -Nama
+// 12S23029 - Sintong Hutapea
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nmGd, aC, lt, ktsTk, hasil;
+        int kpsGd, jmBk, pgKpsGd;
+
+        nmGd = input.nextLine();
+        kpsGd = input.nextInt();
+        jmBk = input.nextInt();
+        aC = input.nextLine();
+        lt = input.nextLine();
+        ktsTk = input.nextLine();
+        pgKpsGd = (int) ((double) jmBk / kpsGd * 100);
+        if (pgKpsGd >= 40) {
+            hasil = "Gudang Elite";
+        } else {
+            if (pgKpsGd >= 25) {
+                hasil = "Gudang Standar";
+            } else {
+                if (pgKpsGd < 25) {
+                    hasil = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang");
+        System.out.println(nmGd + "|" + kpsGd + "|" + jmBk + "|" + aC + "|" + lt + "|" + ktsTk + "|" + pgKpsGd + "|" + hasil);
+    }
+}
